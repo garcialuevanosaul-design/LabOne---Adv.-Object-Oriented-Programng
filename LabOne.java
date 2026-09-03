aa7badd9085cd6fdf0f77c832f240a3814b1abb3
Abdullah Abdmahdi <abdullahabdmahdi@gmail.com>
2026-09-02 19:48:20 -0600

    Added verification result printer

--- LabOne.java
+++ LabOne.java
@@ -3,53 +3,67 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-public class LabOne{
 
-    public static class ChessPiece{
+public class LabOne {
+
+    public static class ChessPiece {
         public String name;
         public String color;
         public char pos_X;
         public int pos_Y;
     }
-    public static boolean kingValidator(char pos_x, int pos_y){
+
+    public static boolean kingValidator(char pos_x, int pos_y) {
         // Logic
     }
 
-    public static boolean rookValidator(char pos_x, int pos_y){
+    public static boolean rookValidator(char pos_x, int pos_y) {
         // Logic
     }
 
-    public static boolean queenValidator(char pos_x, int pos_y){
+    public static boolean queenValidator(char pos_x, int pos_y) {
         // Logic
     }
 
-    public static boolean bishopValidator(char pos_x, int pos_y){
+    public static boolean bishopValidator(char pos_x, int pos_y) {
         // Logic
     }
 
-    public static boolean knightValidator(char pos_x, int pos_y){
+    public static boolean knightValidator(char pos_x, int pos_y) {
         // Logic
     }
 
-    public static boolean pawnValidator(char pos_x, int pos_y){
+    public static boolean pawnValidator(char pos_x, int pos_y) {
         // Logic
     }
 
-    public static void main(String[] args){
+    // Printing Verification:
+    // Prints whether each chess piece can or cannot move to the target position
+    public static void printVerification(ChessPiece piece, char targetX, int targetY, boolean validMove) {
+        if (validMove) {
+            System.out.println(piece.name + " at " + piece.pos_X + ", " + piece.pos_Y + " can move to " + targetX + ", " + targetY);
+        } else {
+            System.out.println(piece.name + " at " + piece.pos_X + ", " + piece.pos_Y + " can NOT move to " + targetX + ", " + targetY);
+        }
+    }
+
+    public static void main(String[] args) {
         List<ChessPiece> chessList = new ArrayList<>(); // I decide to use an ArrayList because it's dynamic
         File file = new File("chess.txt");
         Scanner userInput = new Scanner(System.in);
 
         // Scanner Section:
-        // It has 2 scanners: the first one reads each line, and the second one reads the line to be able to separate it by commas using a delimiter.
-        // It then reads each section of the line, and assign each attribute of the chessPiece object to it, then adds the object to chessList
-        try(Scanner fileScanner = new Scanner(file)){
-            while(fileScanner.hasNextLine()){
+        // It has 2 scanners: the first one reads each line, and the second one reads
+        // the line to be able to separate it by commas using a delimiter.
+        // It then reads each section of the line, and assign each attribute of the
+        // chessPiece object to it, then adds the object to chessList
+        try (Scanner fileScanner = new Scanner(file)) {
+            while (fileScanner.hasNextLine()) {
                 String line = fileScanner.nextLine();
-                try(Scanner rowScanner = new Scanner(line)){
+                try (Scanner rowScanner = new Scanner(line)) {
                     rowScanner.useDelimiter("\\s*,\\s*");
 
-                    if (rowScanner.hasNext()){
+                    if (rowScanner.hasNext()) {
                         ChessPiece piece = new ChessPiece();
 
                         piece.name = rowScanner.next();
@@ -67,9 +81,7 @@ public class LabOne{
             char pos_x = userInput.next().charAt(0);
             int age = userInput.nextInt();
 
-
-
-        }catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("Error: The file could not be found.");
         }
     }

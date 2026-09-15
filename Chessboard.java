74102b4ea1f13af25e2eaed84ec83d5c204a5d52
Pablo Campos <Pcampos2@miners.utep.edu>
2026-09-15 11:28:00 -0600

    Add Chessboard row constants

--- Chessboard.java
+++ Chessboard.java
@@ -1,5 +1,9 @@
 public class Chessboard{
+
+    public static final int MAX_ROW = 8;
+    public static final int MIN_ROW = 1;
+
     public static boolean withinChessboard(char x, int y) {
-        return x >= 'A' && x <= 'H' && y >= 1 && y <= 8;
+        return x >= 'A' && x <= 'H' && y >= MIN_ROW && y <= MAX_ROW;
     }
 }
